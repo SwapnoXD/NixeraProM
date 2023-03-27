ead17fe561286c754de487f929edd5aac7fbb94c
Swapno <109905932+SwapnoXD@users.noreply.github.com>
2023-03-27 12:57:19 +0530

    Create __main__.py

---
+++ Nixera/__main__.py
@@ -0,0 +1,20 @@
+import asyncio
+import importlib
+
+from pyrogram import idle
+from Nixera import LOG
+from Nixera.plugins import ALL_MODULES
+
+
+async def start_bot():
+    for all_module in ALL_MODULES:
+        importlib.import_module("plugins.modules." + all_module)
+    LOG.print("[bold yellow]Vixera Started.")
+    await idle() 
+    LOG.print("[bold red]Vixera Started Successfully.")
+
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(start_bot())
+    
