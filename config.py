d3b56ca50efbff80fef2f2abf6e262f7af60ffa7
Swapno <109905932+SwapnoXD@users.noreply.github.com>
2023-03-27 19:05:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ class Config:
     API_HASH = "a28b981438bfc9bb921563cf09bc4951" #api hash
     BOT_TOKEN = "6255318917:AAFDVg4d5WPDGh49PfK9bpsaM-3vHVkm0Kg" #token of your bot get it from @BotFather
     OWNER_ID = 1626505310 #id of the owner 
+    COMMANDS = [".","?","/","!","$","#","~",]
     START_PIC = "" #not necessary 
